1352104f29d9a9cebac5038f6d9fdde2c63c2610
thiagola92 <thiagola92@gmail.com>
2025-06-29 07:29:53 -0300

    Resize tables works but it's incomplete
    Missing UndoRedo

--- components/table_view/table_bar/table_bar.gd
+++ components/table_view/table_bar/table_bar.gd
@@ -13,7 +13,9 @@ extends HBoxContainer
 
 func _ready() -> void:
 	rows_counter.get_line_edit().focus_exited.connect(_on_line_edit_focus_exited)
+	rows_counter.get_line_edit().text_submitted.connect(_on_line_edit_focus_exited.unbind(1))
 	columns_counter.get_line_edit().focus_exited.connect(_on_line_edit_focus_exited)
+	columns_counter.get_line_edit().text_submitted.connect(_on_line_edit_focus_exited.unbind(1))
 
 
 func set_row_counter(rows_count: int) -> void:
@@ -36,11 +38,24 @@ func _on_line_edit_focus_exited() -> void:
 	var new_columns: int = int(columns_counter.value)
 	
 	if new_rows == current_rows and new_columns == current_columns:
+		# Nothing to do.
 		return
 	
 	if new_rows < current_rows or new_columns < current_columns:
-		# Confirm if can delete rows/columns.
+		# Wait confirmation that can delete rows/columns.
 		return resize_dialog.popup_centered()
 	
 	# Don't need confirmation to add rows/columns.
-	table_view.set_table_size(new_rows, new_columns)
+	_on_resize_dialog_confirmed()
+
+
+func _on_resize_dialog_confirmed() -> void:
+	var current_rows: int = table_view.table.get_rows_count()
+	var current_columns: int = table_view.table.row_columns.get_columns_count()
+	var new_rows: int = int(rows_counter.value)
+	var new_columns: int = int(columns_counter.value)
+	
+	UndoHelper.undo_redo.create_action("Resize table")
+	UndoHelper.undo_redo.add_do_method(table_view.set_table_size.bind(new_rows, new_columns))
+	UndoHelper.undo_redo.add_undo_method(table_view.set_table_size.bind(current_rows, current_columns))
+	UndoHelper.undo_redo.commit_action()
--- windows/resize_dialog/resize_dialog.gd
+++ windows/resize_dialog/resize_dialog.gd
@@ -2,18 +2,5 @@ class_name ResizeDialog
 extends ConfirmationDialog
 
 
-@export var table_bar: TableBar
-
-
 func _ready() -> void:
 	get_ok_button().modulate = Color.html("#c64600")
-
-
-func _on_confirmed() -> void:
-	if not table_bar:
-		return
-	
-	table_bar.table_view.set_table_size(
-		int(table_bar.rows_counter.value),
-		int(table_bar.columns_counter.value)
-	)
